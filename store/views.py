54b0823b592ba955c52532d32dceaf805554d53d
Almazon <d.almaz0140@gmail.com>
2022-09-07 12:59:30 +0600

    Add to cart logic created

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from . import models
+from django.http.response import JsonResponse
+
+import json
 
 
 def store(request):
@@ -38,3 +41,28 @@ def checkout(request):
         'cartItems': cartItems,
     }
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    customer = request.user.customer
+    order, created = models.Order.objects.get_or_create(customer=customer, complete=False)
+    product = models.Product.objects.get(id=productId)
+    orderitem, created = models.OrderItem.objects.get_or_create(
+        order=order,
+        product=product,
+    )
+
+    if action == 'add':
+        orderitem.quantity += 1
+    elif action == 'remove':
+        orderitem.quantity -= 1
+    orderitem.save()
+
+    if orderitem.quantity <= 0:
+        orderitem.delete()
+
+    return JsonResponse(data, safe=False)
